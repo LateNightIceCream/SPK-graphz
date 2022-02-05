b6a18f5111aba1e1a36d0ad7e261ef2480bfa3be
LateNightIceCream <richardgruenert@live.com>
2022-02-05 23:49:29 +0100

    update

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # SPK-graphz
 sparkasse pdfs -> graph
 
+<<<<<<< HEAD
 this was before  I knew you could export csv directly from the website.. :D
 so now it's also
-
+bins
 sparkasse csv -> graph
+=======
+this was before  I knew you could export csv directly from the website.. :d
+Everything is still a little rough
+>>>>>>> d96b01c (update)
 
 ## Features
 - very bad and specific pdf to csv parser
@@ -12,3 +17,6 @@ sparkasse csv -> graph
 
 ## Sample graphs
 ![sample1](sample_1.png)
+just some crude weekday means and histogram
+![sample2](sample_2.png)
+![sample3](sample_3.png)
--- src/spk-graphz-2.R
+++ src/spk-graphz-2.R
@@ -1,22 +1,30 @@
 # this version works with the regular .csv files exported from the web interface
+# TODO: feature: input multiple csv files and overlay them
+# TODO: feature: input multiple keywords and overlay them
 library(ggplot2)
 library(patchwork)
+library(argparser)
+
+parser <- arg_parser('A program to turn Sparkasse .csv files into nice graphs')
+parser <- add_argument(parser, "input", help="input file")
+parser <- add_argument(parser, 'keywords', help='keyword(s) to filter for. Case sensitive. Use \'|\' to OR multiple keywords. Examples: \'LIDL\' or \'LIDL|ALDI\'')
+parser <- add_argument(parser, "--output", help="output (pdf) file")
+
+args <- parse_args(parser)
 
 width   <- 10
 height  <- 0.618 * width
-dat     <- read.csv('bank_statements/2020.csv', sep=';')
+dat     <- read.csv(args$input, sep=';')
 
-# keyword <- 'NETTO|LIDL|Kaufland'
-keyword <- 'LIDL'
-#outname <- paste(keyword, ".pdf", sep="")
-outname <- 'kaufland.pdf'
+keyword <- args$keywords
+outname <- if (is.na(args$output) || args$output == '')  paste(args$keywords, '.pdf', sep='') else args$output
 
 format_data <- function (data, keyword) {
     # only take rows where descr contains keyword
     # TODO: toLower()
     data      <- dplyr::filter(data, grepl(keyword, descr))
     # pls no german decimal ','
-    data$amount <- as.numeric(sub(",", ".", data$amount, fixed = TRUE))
+    data$amount <- as.numeric(sub(',', '.', data$amount, fixed = TRUE))
     data$amount <- abs(data$amount)
     # get R date objects
     data$date <- as.Date(data$date, '%d.%m.%y')
@@ -29,38 +37,35 @@ format_data <- function (data, keyword) {
     data$weekdays <- weekdays(data$date)
     return(data)
 }
-"======================================="
 
 # make format like legacy date,descr,amount
 dat1 <- data.frame(date = dat$Buchungstag, descr=dat$Beguenstigter.Zahlungspflichtiger, amount = dat$Betrag)
 dat1 <- format_data(dat1, keyword)
-#dat2 <- format_data(dat, "MARKANT")
-#dat3 <- format_data(dat, "MARKANT")
 
 start_date <- dat1$date[1]
 end_date   <- tail(dat1$date, n=1)
 
 avg_amount <- mean(dat1$amount)
 
-col1    <- "gray"
-col2    <- "#fa5252"
-col3    <- "#339af0"
-textcol <- "#212529"
+col1    <- 'gray'
+col2    <- '#fa5252'
+col3    <- '#339af0'
+textcol <- '#212529'
 
 right_axis_factor <- max(dat1$cum_amount) / max(dat1$amount)
 
 geom_amount_timeplot <- function (data = dat1, color = col2, color2 = col3) {
   avg_amount <- mean(data$amount)
   list(
-    geom_line(data = data, color="gray"),
+    geom_line(data = data, color='gray'),
     geom_point(data = data, color=color),
     #geom_point(data = data, aes(x = date, y=amount, colour = amount)),
     #scale_colour_gradient(low = color2, high = color),
     geom_line(data = data, aes(x=date, y=cum_amount/right_axis_factor), color=color),
-    geom_hline(yintercept=avg_amount, linetype="dashed", color=color),
+    geom_hline(yintercept=avg_amount, linetype='dashed', color=color),
     #geom_text(label=data$amount),
     # just for testing, date[4] is not very secure ;D
-    annotate("text", x = data$date[4], y = avg_amount, color = textcol, label=sprintf("%.2f", avg_amount))
+    annotate('text', x = data$date[4], y = avg_amount, color = textcol, label=sprintf('%.2f', avg_amount))
     )
 }
 
@@ -68,21 +73,20 @@ geom_amount_timeplot <- function (data = dat1, color = col2, color2 = col3) {
 p <- ggplot(data = dat1, aes(x=date, y=amount)) +
   theme_minimal() +
   ggtitle(
-    label = paste(keyword, "expenses"),
-    subtitle = paste("from", start_date, "to", end_date)
+    label = paste(keyword, 'expenses'),
+    subtitle = paste('from', start_date, 'to', end_date)
   ) +
-  theme(plot.title=element_text(color="black"),
-          plot.subtitle=element_text(color="gray")) +
+  theme(plot.title=element_text(color='black'),
+          plot.subtitle=element_text(color='gray')) +
   scale_y_continuous(
     # Features of the first axis
-    name = "amount / EUR",
+    name = 'amount / EUR',
     # Add a second axis and specify its features
-    sec.axis = sec_axis( trans=~.*right_axis_factor, name="cumulative amount / EUR"),
+    sec.axis = sec_axis( trans=~.*right_axis_factor, name='cumulative amount / EUR'),
     breaks = scales::pretty_breaks(n = 6)
   ) +
   geom_amount_timeplot()
-  #geom_amount_timeplot(data=dat2, color="red")
-  #geom_amount_timeplot(data=dat3, color="purple")
+  #geom_amount_timeplot(data=dat2, color='red')
 
 # weekday plot
 w <- ggplot(data = aggregate(dat1$amount, list(dat1$weekdays), mean), aes(Group.1, x), ) +
@@ -95,4 +99,4 @@ h <- ggplot(data = dat1, aes(x=amount)) +
   geom_histogram()
 
 pdf(outname, width, height)
-p
+h
